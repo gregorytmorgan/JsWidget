9ae79d48ac97fb335af36adadb89d9fe716a4a1d
Greg Morgan <gregorytmorgan@gmail.com>
2015-07-14 11:41:33 -0600

    Update bad data test to work with JSON response.

--- time.php
+++ time.php
@@ -1,62 +1,60 @@
 <?php
 
-	// default result
-	$defaultResult = array(
-		"status" => array(
-			"code" => 400,
-			"text" => "error",
-			"description" => "there_was_an_error"
-		),
-		"data" => null
-	);
+// default result
+$defaultResult = array(
+	"status" => array(
+		"code" => 400,
+		"text" => "error",
+		"description" => "there_was_an_error"
+	),
+	"data" => null
+);
 
-	// randomly delay the response - max should be longer than client-side request 
-	// timeout to test timeout handling on the client side
-	sleep(rand(0,8));
+// randomly delay the response - max should be longer than client-side request
+// timeout to test timeout handling on the client side
+sleep(rand(0, 8));
 
-//	// break a specific caller
-//	if ($_POST["name"] === 'two') {
-//		 echo 'xa:sb:cz';
-//		 return;
-//	}
+$val = rand(0, 9);
 
-	$val = rand(0,9);
+// return invalid data
+if ($val == 9) {
+	echo "{adfdajajdfkj}";
+	return;
+}
 
-	// return invalid data
-	if ($val == 9) {
-		echo "{adfdajajdfkj}";
-		return;
-	}
+// return no data
+if ($val == 8) {
+	return;
+}
 
-	// return no data
-	if ($val == 8) {
-		return;
-	}
-	
-	// return fail response code
-    if ($val == 7) {
-		http_response_code(404);
-		return;
-	}
-	
-	// success result
-	$result = array(
-			"status" => array(
-			"code" => 200,
-			"text" => "ok",
-			"description" => "success"	
-		),
-		"data" => date("H:i:s")
-	);	
-			
-	$jsonResult = json_encode($result);
-			
-	if ($jsonResult === false) {
-		$defaultResult["status"]["description"] = "json_encode_error";
-		echo json_encode($defaultResult);
-		return;
-	}
-	
-	echo $jsonResult;
-	
-	
+// return fail response code
+if ($val == 7) {
+	http_response_code(404);
+	return;
+}
+
+// success result
+$result = array(
+	"status" => array(
+		"code" => 200,
+		"text" => "ok",
+		"description" => "success"
+	),
+	"data" => date("H:i:s")
+);
+
+// break a specific caller
+//if ($_POST["name"] === 'two') {
+//	$result["data"] = "xa:sb:cz";
+//	return;
+//}
+
+$jsonResult = json_encode($result);
+
+if ($jsonResult === false) {
+	$defaultResult["status"]["description"] = "json_encode_error";
+	echo json_encode($defaultResult);
+	return;
+}
+
+echo $jsonResult;
